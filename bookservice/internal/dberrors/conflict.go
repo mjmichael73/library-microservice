53c2ae1d30ca9df7a2bc62e10243d26a2df3ad30
Mojtaba Michael <mojimich2015@gmail.com>
2025-02-10 23:14:07 +0300

    Formatted code using go fmt

--- bookservice/internal/dberrors/conflict.go
+++ bookservice/internal/dberrors/conflict.go
@@ -1,6 +1,6 @@
 package dberrors
 
-type ConflictError struct {}
+type ConflictError struct{}
 
 func (e *ConflictError) Error() string {
 	return "attempted to create a record with an existing key"
